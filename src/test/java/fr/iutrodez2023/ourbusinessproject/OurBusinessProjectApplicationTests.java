896728d19f724816404e35429c151317008b18ed
Franck Silvestre <franck.silvestre@gmail.com>
2023-02-01 10:42:18 +0100

    Update POM

---
+++ src/test/java/fr/iutrodez2023/ourbusinessproject/OurBusinessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iutrodez2023.ourbusinessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurBusinessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
