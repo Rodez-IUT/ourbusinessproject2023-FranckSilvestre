59a33d3a5b904fb9b180c512eb93709c34732f1c
Franck Silvestre <franck.silvestre@gmail.com>
2023-02-06 15:00:59 +0100

    fix #1.2 classe Enterprise

---
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -0,0 +1,50 @@
+package ourbusinessproject;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+public class Enterprise {
+    @NotEmpty
+    private String name;
+    @Size(min = 10)
+    private String description;
+    @NotEmpty
+    private String contactName;
+    @Email @NotEmpty
+    private String contactEmail;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+}
